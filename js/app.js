5f8e0b6acb66a2f0821d7643f76e4c6750eca406
michailid <michailidoumaria224@gmail.com>
2026-04-24 16:12:59 +0300

    game functionality & winner identification

--- js/app.js
+++ js/app.js
@@ -1,18 +1,122 @@
 const App = {
-    // All of our selected HTML elements
-    $: {
-        menu: document.querySelector('.menu'),
-        menuItems: document.querySelector('.items'),
-        icon: document.querySelector('.menu i'),
-    },
+  // All of our selected HTML elements
+  $: {
+    menu: document.querySelector('[data-id="menu"]'),
+    menuItems: document.querySelector('[data-id="menu-items"]'),
+    icon: document.querySelector(".menu i"),
+    resetBtn: document.querySelector('[data-id="reset-btn"'),
+    newRoundBtn: document.querySelector('[data-id="new-round-btn"'),
+    squares: document.querySelectorAll('[data-id="square"'),
+  },
 
-    init() {
-        App.$.menu.addEventListener("click", (event) => {
-          App.$.menuItems.classList.toggle('hidden');
-          App.$.icon.classList.toggle('fa-chevron-down');
-          App.$.icon.classList.toggle('fa-chevron-up');
+  state: {
+    moves: [],
+  },
+
+  getGameStatus(moves) {
+    const p1Moves = moves
+      .filter((move) => move.playerId === 1)
+      .map((move) => +move.squareId);
+    const p2Moves = moves
+      .filter((move) => move.playerId === 2)
+      .map((move) => +move.squareId);
+    console.log(p1Moves);
+
+    const winningPatterns = [
+      [1, 2, 3],
+      [1, 5, 9],
+      [1, 4, 7],
+      [2, 5, 8],
+      [3, 5, 7],
+      [3, 6, 9],
+      [4, 5, 6],
+      [7, 8, 9],
+    ];
+    let winner = null;
+    winningPatterns.forEach((pattern) => {
+      const p1Wins = pattern.every((v) => p1Moves.includes(v));
+      const p2Wins = pattern.every((v) => p2Moves.includes(v));
+      if (p1Wins) winner = 1;
+      if (p2Wins) winner = 2;
+    });
+
+    return {
+      status: moves.length === 9 || winner != null ? "complete" : "in-progress", // in-progress | complete
+      winner: winner, // 1 | 2 | null
+    };
+  },
+
+  init() {
+    App.registerEventListeners();
+  },
+
+  registerEventListeners() {
+    // DONE
+    App.$.menu.addEventListener("click", (event) => {
+      App.$.menuItems.classList.toggle("hidden");
+      App.$.icon.classList.toggle("fa-chevron-down");
+      App.$.icon.classList.toggle("fa-chevron-up");
+    });
+
+    // TODO
+    App.$.resetBtn.addEventListener("click", (event) => {
+      console.log("Reset the game");
+    });
+
+    // TODO
+    App.$.newRoundBtn.addEventListener("click", (event) => {
+      console.log("Add a new round");
+    });
+
+    // TODO
+    App.$.squares.forEach((square) => {
+      square.addEventListener("click", (event) => {
+        // Check if there is already a play, if so, return early
+        const hasMove = (squareId) => {
+          const existingMove = App.state.moves.find(
+            (move) => move.squareId === squareId,
+          );
+          return existingMove !== undefined;
+        };
+
+        if (hasMove(+square.id)) {
+          return;
+        }
+
+        // Determine which player icon to add to the square
+        const lastMove = App.state.moves.at(-1);
+        const getOppositePlayer = (playerId) => (playerId == 1 ? 2 : 1);
+        const currentPlayer =
+          App.state.moves.length === 0
+            ? 1
+            : getOppositePlayer(lastMove.playerId);
+
+        const icon = document.createElement("i");
+        if (currentPlayer === 1) {
+          icon.classList.add("fa-solid", "fa-x", "yellow");
+        } else {
+          icon.classList.add("fa-solid", "fa-o", "turquoise");
+        }
+
+        App.state.moves.push({
+          squareId: +square.id,
+          playerId: currentPlayer,
         });
-    },
+        console.log(App.state);
+        square.replaceChildren(icon);
+
+        // Check if there is a winner or tie game
+        const game = App.getGameStatus(App.state.moves);
+        if (game.status === "complete") {
+          if (game.winner) {
+            alert(`Player ${game.winner} wins!`);
+          } else {
+            alert("Tie!");
+          }
+        }
+      });
+    });
+  },
 };
 
-window.addEventListener('load', App.init);
+window.addEventListener("load", App.init);
