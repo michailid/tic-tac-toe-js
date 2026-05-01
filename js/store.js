e6eb1bb5e499ffb3f6581f58ac9b735508b687f6
michailid <michailidoumaria224@gmail.com>
2026-05-01 19:26:34 +0300

    show score on scoreboard & new round btn functionality

--- js/store.js
+++ js/store.js
@@ -1,5 +1,9 @@
 const initialValue = {
-  moves: [],
+  currentGameMoves: [],
+  history: {
+    currentRoundGames: [],
+    allGames: [],
+  },
 };
 
 export default class Store {
@@ -9,10 +13,28 @@ export default class Store {
     this.players = players;
   }
 
+  get stats() {
+    const state = this.#getState();
+    return {
+      playerWithStats: this.players.map((player) => {
+        const wins = state.history.currentRoundGames.filter(
+          (game) => game.status.winner?.id === player.id,
+        ).length;
+        return {
+          ...player,
+          wins,
+        };
+      }),
+      ties: state.history.currentRoundGames.filter(
+        (game) => game.status.winner == null,
+      ).length,
+    };
+  }
+
   // getter
   get game() {
     const state = this.#getState();
-    const currentPlayer = this.players[state.moves.length % 2];
+    const currentPlayer = this.players[state.currentGameMoves.length % 2];
 
     const winningPatterns = [
       [1, 2, 3],
@@ -27,7 +49,7 @@ export default class Store {
 
     let winner = null;
     for (const player of this.players) {
-      const selectedSquareIds = state.moves
+      const selectedSquareIds = state.currentGameMoves
         .filter((move) => move.player.id === player.id)
         .map((move) => move.squareId);
 
@@ -39,19 +61,18 @@ export default class Store {
     }
 
     return {
-      moves: state.moves,
+      moves: state.currentGameMoves,
       currentPlayer,
       status: {
-        isComplete: winner != null || state.moves.length === 9,
+        isComplete: winner != null || state.currentGameMoves.length === 9,
         winner,
       },
     };
   }
 
   playerMove(squareId) {
-    const state = this.#getState();
-    const stateClone = structuredClone(state);
-    stateClone.moves.push({
+    const stateClone = structuredClone(this.#getState());
+    stateClone.currentGameMoves.push({
       squareId,
       player: this.game.currentPlayer,
     });
@@ -60,7 +81,26 @@ export default class Store {
   }
 
   reset() {
-    this.#saveState(initialValue);
+    const stateClone = structuredClone(this.#getState());
+    const { status, moves } = this.game;
+    if (status.isComplete) {
+      stateClone.history.currentRoundGames.push({
+        moves,
+        status,
+      });
+    }
+
+    stateClone.currentGameMoves = [];
+
+    this.#saveState(stateClone);
+  }
+
+  newRound() {
+    this.reset();
+    const stateClone = structuredClone(this.#getState());
+    stateClone.history.allGames.push(...stateClone.history.currentRoundGames);
+    stateClone.history.currentRoundGames = [];
+    this.#saveState(stateClone);
   }
 
   #getState() {
--- js/view.js
+++ js/view.js
@@ -14,6 +14,10 @@ export default class View {
     this.$.modalText = this.#qs('[data-id="modal-text"]');
     this.$.modalBtn = this.#qs('[data-id="modal-btn"]');
     this.$.turn = this.#qs('[data-id="turn"]');
+    this.$.p1Wins = this.#qs('[data-id="p1-wins"]');
+    this.$.p2Wins = this.#qs('[data-id="p2-wins"]');
+    this.$.ties = this.#qs('[data-id="ties"]');
+
 
     this.$$.squares = this.#qsAll('[data-id="square"]');
 
@@ -45,6 +49,13 @@ export default class View {
   /*
   DOM helper methods
   */
+  updateScoreboard(p1Wins, p2Wins, ties) {
+    this.$.p1Wins.innerText = `${p1Wins} wins`;
+    this.$.p2Wins.innerText = `${p2Wins} wins`;
+    this.$.ties.innerText = `${ties} ties`;
+
+  }
+
   openModal(message) {
     this.$.modal.classList.remove("hidden");
     this.$.modalText.innerText = message;
